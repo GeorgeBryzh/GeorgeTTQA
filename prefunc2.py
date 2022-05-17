600f1ad7a2cced538de272d4697b5ea2761a4f87
George <vortanov20@gmail.com>
2022-05-17 22:10:37 +0300

    index.html mod

--- prefunc2.py
+++ prefunc2.py
@@ -3,3 +3,4 @@ def testTask2():
  con = sqlite3.connect('sqlite/Countries.db')
  cur = con.cursor()
  assert len(list(cur.execute("SELECT * FROM Countries WHERE Population/Area < 50 AND Country !='USA'")))==0 and len(list(cur.execute("SELECT * FROM Countries WHERE Population/Area < 50 AND Country =='USA'")))!=0
+ffff
